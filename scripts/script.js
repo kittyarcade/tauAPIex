fd3451452b1dddfce2ea1f391a31ad83e91897ec
Amanda Jopp <aclaire488@gmail.com>
2016-11-30 22:24:10 -0600

    initial js complete

---
+++ scripts/script.js
@@ -0,0 +1,41 @@
+console.log('js');
+
+
+$(document).ready(function(){
+
+  $(document).on('click', '#searchButton', function(){
+
+    var searchTitle = $('#searchIn').val();
+    console.log('searching for: ', searchTitle);
+
+    var searchURL = 'http://www.omdbapi.com/?s=' + searchTitle;
+
+
+    $.ajax({
+      url: searchURL,
+      dataType: 'JSON',
+      success: function (data){
+        console.log( 'data:', data );
+
+        showResults(data.Search);
+      },
+
+      statusCode: {
+        404: function(){
+          alert('error connecting to server');
+        }
+      }
+    });
+  });//end click button
+
+  var showResults = function (results) {
+    console.log('in showResults', results);
+
+$('#outputDiv').empty();
+//now loop through results and display movies
+for(var i = 0; i < results.length; i++){
+  $('#outputDiv').append('<p><b>' + results[i].Title + '<b>(' + results[i].Year+')</p>');
+  $('#outputDiv').append('<img src="' + results[i].Poster + '">');
+}//end for
+};
+});//end show results
